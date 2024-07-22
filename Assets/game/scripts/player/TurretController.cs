64e9eb6ec090f19daf31cf79ebbd362f6aa4bdb5
emiliagit <94327724+emiliagit@users.noreply.github.com>
2024-07-22 12:54:57 -0300

    cinemachine

--- Assets/game/scripts/player/TurretController.cs
+++ Assets/game/scripts/player/TurretController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class TurretController : MonoBehaviour
 {
-    public float rotationSpeed = 5f;  // Velocidad de rotación de la torreta
-    public Transform turret;          // Referencia al transform de la torreta
+    //public float rotationSpeed = 5f;  // Velocidad de rotación de la torreta
+    //public Transform turret;          // Referencia al transform de la torreta
+
+    [SerializeField] private float spinSpeed;
 
     void Update()
     {
@@ -14,21 +16,30 @@ public class TurretController : MonoBehaviour
 
     void RotateTurret()
     {
-        // Obtiene la posición del mouse en el mundo
-        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics.Raycast(ray, out RaycastHit hit))
+        //// Obtiene la posición del mouse en el mundo
+        //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if (Physics.Raycast(ray, out RaycastHit hit))
+        //{
+        //    Vector3 targetPosition = hit.point;
+
+        //    // Calcula la dirección hacia la cual debe girar la torreta
+        //    Vector3 direction = targetPosition - turret.position;
+        //    direction.y = 0;  // Mantén la rotación en el plano horizontal
+
+        //    // Calcula la rotación objetivo
+        //    Quaternion targetRotation = Quaternion.LookRotation(direction);
+
+        //    // Interpola la rotación actual hacia la rotación objetivo
+        //    turret.rotation = Quaternion.Lerp(turret.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+        //}
+
+        if (Input.GetMouseButton(4))
         {
-            Vector3 targetPosition = hit.point;
-
-            // Calcula la dirección hacia la cual debe girar la torreta
-            Vector3 direction = targetPosition - turret.position;
-            direction.y = 0;  // Mantén la rotación en el plano horizontal
-
-            // Calcula la rotación objetivo
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-
-            // Interpola la rotación actual hacia la rotación objetivo
-            turret.rotation = Quaternion.Lerp(turret.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+            transform.Rotate(Vector3.up, -spinSpeed * Time.deltaTime);
+        }
+        if (Input.GetMouseButton(5))
+        {
+            transform.Rotate(Vector3.up, spinSpeed * Time.deltaTime);
         }
     }
 }
